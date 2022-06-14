d859c573eb150f3f8a4318eea45ce5ab279426b3
TikhomirovaElvira <elviratikhomirova@mail.ru>
2022-06-14 22:39:13 +0300

    дз 2.7 Коллекции: многообразие реализаций

--- src/ru/skypro/hw-2.5/src/main/java/pro/sky/hw_2_5/EmployeeService.java
+++ src/ru/skypro/hw-2.5/src/main/java/pro/sky/hw_2_5/EmployeeService.java
@@ -2,25 +2,26 @@ package pro.sky.hw_2_5;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Service
 public class EmployeeService {
 
     private static final int LIMIT = 10;
 
-    private final List<Employee> employees = new ArrayList<>(10);
+    private final Map<String, Employee> employees = new HashMap<>(10);
+
+    private String getKey(Employee employee) {
+        return employee.getFirstName() + " " + employee.getLastName();
+    }
 
     public Employee add(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if (employees.contains(employee)) {
+        if (employees.containsKey(getKey(employee))) {
             throw new EmployeeAlreadyAddedException();
         }
         if (employees.size() < LIMIT) {
-            employees.add(employee);
+            employees.put(getKey(employee), employee);
             return employee;
         }
         throw new EmployeeStorageIsFullException();
@@ -28,23 +29,24 @@ public class EmployeeService {
     }
     public Employee remove(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-            if (!employees.contains(employee)) {
+        String key = getKey(employee);
+            if (!employees.containsKey(key)) {
                 throw new EmployeeNotFoundException();
             }
-            employees.remove(employee);
+            employees.remove(key);
             return employee;
     }
 
     public Employee find (String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-            if (!employees.contains(employee)) {
+            if (!employees.containsKey(getKey(employee))) {
                 throw new EmployeeNotFoundException();
             }
                 return employee;
     }
 
     public List<Employee> getAll() {
-        return new ArrayList<>(employees);
+        return new ArrayList<>(employees.values());
     }
 
 }
